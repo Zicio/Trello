3d6124346f44402d583b5913602c40578e78c0ed
Alexander Burachkov <zicio@yandex.ru>
2021-12-21 23:41:00 +0400

    Updated

--- src/js/Dom.js
+++ src/js/Dom.js
@@ -1,4 +1,24 @@
 export default class Dom {
+  static renderSavedCards() {
+    const lists = document.getElementsByClassName('column__list');
+    for (const list of [...lists]) {
+      const columnName = list.closest('.column').classList[1];
+      const savedData = JSON.parse(localStorage.getItem(columnName));
+      if (savedData) {
+        for (const el of savedData) {
+          const newCard = document.createElement('li');
+          newCard.classList.add('column__card');
+          newCard.textContent = el;
+          const cross = document.createElement('button');
+          cross.classList.add('button', 'card__delete');
+          cross.textContent = '✖';
+          list.appendChild(newCard);
+          newCard.appendChild(cross);
+        }
+      }
+    }
+  }
+
   static renderAddCardMenu(e) {
     const column = e.target.closest('.column');
     const formBox = column.querySelector('.form__box');
@@ -14,17 +34,20 @@ export default class Dom {
     const newCard = document.createElement('li');
     newCard.classList.add('column__card');
     newCard.textContent = text;
-    newCard.setAttribute('tabindex', 1);
     const cross = document.createElement('button');
     cross.classList.add('button', 'card__delete');
     cross.textContent = '✖';
     list.appendChild(newCard);
     newCard.appendChild(cross);
     Dom.renderAddCardMenu(e);
+    Dom.saveData(column, newCard);
   }
 
   static renderCross(e) {
-    const cross = e.target.querySelector('.card__delete');
+    let cross = e.target.querySelector('.card__delete');
+    if (cross === null) {
+      cross = e.target;
+    }
     cross.classList.toggle('active');
   }
 
@@ -32,5 +55,18 @@ export default class Dom {
     const card = e.target.parentNode;
     const columnList = e.target.closest('.column__list');
     columnList.removeChild(card);
+  }
+
+  static saveData(column, newCard) {
+    const columnName = column.classList[1];
+    if (localStorage.getItem(columnName)) {
+      const arr = JSON.parse(localStorage.getItem(columnName));
+      arr.push(newCard.innerText);
+      localStorage.setItem(`${columnName}`, JSON.stringify(arr));
+      return;
+    }
+    const arr = [];
+    arr.push(newCard.innerText);
+    localStorage.setItem(`${columnName}`, JSON.stringify(arr));
   }
 }
--- src/js/Logic.js
+++ src/js/Logic.js
@@ -4,9 +4,17 @@ export default class Logic {
   constructor(element) {
     this.element = element;
 
+    Logic.start();
     this.listenerOfClickButton();
     this.listenerOfTargetCard();
     this.listenerOfUntargetCard();
+    this.movingCard();
+  }
+
+  static start() {
+    if (localStorage.length) {
+      Dom.renderSavedCards();
+    }
   }
 
   listenerOfClickButton() {
@@ -14,16 +22,20 @@ export default class Logic {
   }
 
   static renderElement(e) {
+    // Меню добавления карточки
     if (e.target.classList.contains('column__add-element') || e.target.classList.contains('form__remove')) {
       e.preventDefault();
       Dom.renderAddCardMenu(e);
       return;
     }
+    // Добавление карточки
     if (e.target.classList.contains('form__submit')) {
       e.preventDefault();
       Dom.renderNewCard(e);
+      // Logic.saveData(e);
       return;
     }
+    // Удаление карточки
     if (e.target.classList.contains('card__delete')) {
       e.preventDefault();
       Dom.deleteCard(e);
@@ -39,9 +51,77 @@ export default class Logic {
   }
 
   static renderCross(e) {
-    if (e.target.classList.contains('column__card')) {
+    if (e.target.closest('.column__card')) {
       e.preventDefault();
       Dom.renderCross(e);
     }
+  }
+
+  movingCard() {
+    let draggedItem = null;
+    let selectedItem = null;
+    let shiftX = null;
+    let shiftY = null;
+    this.element.addEventListener('mousedown', (e) => {
+      if (e.target.classList.contains('column__card')) {
+        e.preventDefault();
+        const target = e.target.closest('.column__card');
+        selectedItem = target;
+        selectedItem.classList.add('column__card_selected');
+
+        draggedItem = target.cloneNode(true);
+        draggedItem.classList.add('column__card_dragged');
+
+        const width = selectedItem.clientWidth;
+        const height = selectedItem.clientHeight;
+
+        shiftX = e.clientX - selectedItem.getBoundingClientRect().left;
+        shiftY = e.clientY - selectedItem.getBoundingClientRect().top;
+
+        draggedItem.style.width = `${width}px`;
+        draggedItem.style.height = `${height}px`;
+        draggedItem.style.top = `${e.pageY - shiftY}px`;
+        draggedItem.style.left = `${e.pageX - shiftX}px`;
+        draggedItem.style.listStyle = 'none';
+        draggedItem.style.transform = 'rotate(5deg)';
+
+        this.element.appendChild(draggedItem);
+      }
+    });
+
+    this.element.addEventListener('mousemove', (e) => {
+      if (!selectedItem) {
+        return;
+      }
+
+      draggedItem.style.top = `${e.pageY - shiftY}px`;
+      draggedItem.style.left = `${e.pageX - shiftX}px`;
+    });
+
+    this.element.addEventListener('mouseup', (e) => {
+      if (!selectedItem) {
+        return;
+      }
+
+      const x = e.clientX;
+      const y = e.clientY;
+
+      draggedItem.style.display = 'none';
+      const changingItem = document.elementFromPoint(x, y);
+      const { top } = changingItem.getBoundingClientRect();
+      const parent = changingItem.closest('.column').querySelector('.column__list');
+      if ((e.pageY > window.scrollY + top + changingItem.offsetHeight / 2)
+      && (changingItem.nextElementSibling)) {
+        parent.insertBefore(selectedItem, changingItem.nextElementSibling);
+      } else {
+        parent.insertBefore(selectedItem, changingItem);
+      }
+
+      selectedItem.classList.remove('column__card_selected');
+      selectedItem = null;
+
+      draggedItem.remove();
+      draggedItem = null;
+    });
   }
 }
